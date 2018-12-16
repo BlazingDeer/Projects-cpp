8153fa6d497413282c81fbd4e5fe3ba5388aef49
BlazingDeer <45914520+BlazingDeer@users.noreply.github.com>
2018-12-16 15:20:54 +0100

    Added some code to GaussEl

--- GaussElimination/GaussElimination/src/Źródło.cpp
+++ GaussElimination/GaussElimination/src/Źródło.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <iomanip>
 
+class GaussElimination
+{
+private:
+	int n;
+	std::vector<std::vector<double>> matrix;
+public:
+	GaussElimination(int c) : matrix(c, std::vector<double>(c)), n(c)
+	{
+		EnterMatrix();
+		ShowMatrix();
+	}
+
+
+	void EnterMatrix()
+	{
+		int count = 0;
+		for (auto &i : matrix)
+		{
+			std::cout << "\nwprowadz elementy " << count << " wiersza:\n";
+			
+			for (auto &k : i)
+			{
+				std::cin >> k;
+			}
+			++count;
+		}
+	}
+
+	void ShowMatrix()
+	{
+		for (auto &i : matrix)
+		{
+			int count = 0;
+			std::cout << "\n| ";
+			for (auto &k : i)
+			{
+				std::cout << std::setw(8) << std::setprecision(4) << k;
+			}
+			std::cout << "        |";
+			++count;
+		}
+		std::cout << "\n";
+	}
+};
 
 
 int main()
 {
+	int n;
+	std::cout << "\nWprowadz stopien macierzy: n= ";
+	std::cin >> n;
+	GaussElimination matrix(n);
 
 
+
+	std::cout << "\n\n";
 	system("pause");
 	return 0;
 }
