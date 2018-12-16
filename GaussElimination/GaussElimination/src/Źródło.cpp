a63281214bbca0cc105112956df95f5262004ca6
BlazingDeer <45914520+BlazingDeer@users.noreply.github.com>
2018-12-16 23:36:23 +0100

    Created Basic Gauss Elimination Method

--- GaussElimination/GaussElimination/src/Źródło.cpp
+++ GaussElimination/GaussElimination/src/Źródło.cpp
@@ -2,59 +2,164 @@
 #include <vector>
 #include <cstdlib>
 #include <iomanip>
+#include <cmath>
 
 class GaussElimination
 {
 private:
 	int n;
-	std::vector<std::vector<double>> matrix;
+	double **matrix;
+	double  *matrixValues;
+	double *BasicGaussWyniki;
 public:
-	GaussElimination(int c) : matrix(c, std::vector<double>(c)), n(c)
+
+
+	GaussElimination()
 	{
+		CreateMatrix();
 		EnterMatrix();
-		ShowMatrix();
+		ShowMatrix(matrix,matrixValues);
 	}
+
+
+
+	~GaussElimination()
+	{
+		
+			for (int i = 0; i < n; i++)
+			{
+				free(matrix[i]);
+			}
+		free(matrix);
+	}
+
+
+	void CreateMatrix()
+	{
+		std::cout << "\nWprowadz stopien macierzy: n= ";
+		std::cin >> n;
+		matrix = (double**)calloc(n, sizeof(double*));
+		for (int i = 0; i < n; ++i)
+		{
+			matrix[i] = (double*)calloc(n, sizeof(double));
+		}
+		matrixValues = (double *)(calloc(n, sizeof(double)));
+		BasicGaussWyniki = (double *)(calloc(n, sizeof(double)));
+
+	}
+
 
 
 	void EnterMatrix()
 	{
 		int count = 0;
-		for (auto &i : matrix)
+		for (int i = 0; i < n; ++i)
 		{
 			std::cout << "\nwprowadz elementy " << count << " wiersza:\n";
 			
-			for (auto &k : i)
+			for (int k=0;k<n;k++)
 			{
-				std::cin >> k;
+				std::cin >> matrix[i][k];
 			}
 			++count;
 		}
+
+		std::cout << "\nwprowadz macierz wynikow:\n" ;
+		for (int i=0;i<n;i++)
+		{
+			
+			std::cin >> matrixValues[i];
+			
+			
+		}
+
 	}
 
-	void ShowMatrix()
+	void ShowMatrix(double** toshow, double* values)
 	{
-		for (auto &i : matrix)
+		
+		for (int i=0;i<n;i++)
 		{
-			int count = 0;
+			
 			std::cout << "\n| ";
-			for (auto &k : i)
+			for (int k=0; k<n; k++)
 			{
-				std::cout << std::setw(8) << std::setprecision(4) << k;
+				std::cout << std::setw(8) << std::setprecision(4) << toshow[i][k];
 			}
-			std::cout << "        |";
-			++count;
+			std::cout << "        |" << "|"<<std::setw(8) << values[i];
+			
 		}
 		std::cout << "\n";
+	}
+
+
+
+
+
+	void BasicGaussElimination()
+	{
+		double** BasicMatrix = matrix;
+		double* BasicMatrixValues = matrixValues;
+		for (int i=0; i<n-1;i++)
+		{
+			if (BasicMatrix[i][i] == 0)
+			{
+				std::cout << "\nBlad wartosc po przekatnej rowna 0!";
+				break;
+
+			}
+
+			for (int wdol = i+1; wdol < n; wdol++)
+			{
+				double temp = BasicMatrix[wdol][i] / BasicMatrix[i][i];
+				
+				for (int wbok = i ; wbok < n; wbok++)
+				{
+					BasicMatrix[wdol][wbok] -= BasicMatrix[i][wbok] * temp;
+
+					if (abs(BasicMatrix[wdol][wbok]) <= 1e-12) BasicMatrix[wdol][wbok] = 0;
+				
+				}
+
+				BasicMatrixValues[wdol] -= temp * BasicMatrixValues[i];
+
+
+
+			}
+
+
+
+
+
+		
+		}
+		std::cout << "\n\nPodstawowy Gauss\n";
+		ShowMatrix(BasicMatrix, BasicMatrixValues);
+
+		BasicGaussWyniki[n - 1] = BasicMatrixValues[n - 1] / BasicMatrix[n - 1][n - 1];
+		for (int i = n - 2; i >= 0; i--)
+		{
+			for (int k = n-1; k > i; k--)
+			{
+				BasicMatrixValues[i] -= BasicMatrix[i][k] * BasicGaussWyniki[k];
+				BasicMatrix[k] = 0;
+			}
+			BasicGaussWyniki[i]= BasicMatrixValues[i] / BasicMatrix[i][i];
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << "\nX" << i << "=" << BasicGaussWyniki[i];
+		}
 	}
 };
 
 
 int main()
 {
-	int n;
-	std::cout << "\nWprowadz stopien macierzy: n= ";
-	std::cin >> n;
-	GaussElimination matrix(n);
+	GaussElimination matrix;
+	matrix.BasicGaussElimination();
+
 
 
 
